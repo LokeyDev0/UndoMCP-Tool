d5fa8d7fdea5a22d9c9a0c0659b90fba0470a25a
Lokus <techgaming1908@gmail.com>
2026-07-13 09:04:55 +0530

    Remove dist

--- dist/commands/uninstall.d.ts
+++
@@ -1,5 +0,0 @@
-export interface UninstallOptions {
-    keepDb?: boolean;
-    all?: boolean;
-}
-export declare function runUninstall(options: UninstallOptions): Promise<void>;
--- legacy/utils/tui.d.ts
+++
@@ -1,3 +0,0 @@
-import { DatabaseManager } from '../journal/database-manager.js';
-import { UndoController } from '../undo/undo-controller.js';
-export declare function runTui(dbManager: DatabaseManager, undoController: UndoController, sessionId: string): Promise<void>;
